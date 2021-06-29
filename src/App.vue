2adc12c8988cf3319487b52d057d028e61a69b8f
minsang <whsrpdla@naver.com>
2021-06-29 18:44:34 +0900

    Unit Test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your ParkMinsang App" />
+  <HelloWorld msg="Unit Test Success" />
 </template>
 
 <script>
